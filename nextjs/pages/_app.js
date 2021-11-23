064e29858c4bf09b494ad5a49e8e0abfd9a90bb5
naufalzhafran <naufal.latif@shopee.com>
2021-11-23 20:46:38 +0700

    Add translated project

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+import '../styles/reset.css'
+import '../styles/normalize.css'
+import '../styles/reset-button.css'
+import '../styles/home.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
